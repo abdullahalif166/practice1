894d71d1a28c94cf40bf847f430de486d825943c
unknown <abdullah.alif.166@gmail.com>
2023-08-20 16:36:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function addRow(){
+    var shopName = document.getElementById('shopName').value;
+    var address = document.getElementById('address').value;
+    var ownerName = document.getElementById('ownerName').value;
+    var monogram = document.getElementById('monogram').value;
+    
+    var table = document.getElementById('listTable');
+    var row = table.insertRow(1);
+}
